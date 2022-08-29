fd685750872b6adbb6e3cc691b5ec5e1eb036968
LuckyDL21 <58621656+LuckyDL21@users.noreply.github.com>
2022-08-29 04:24:09 +0000

    이상한문자만들기

--- Programmers/이상한문자만들기.py
+++ Programmers/이상한문자만들기.py
@@ -1,33 +1,14 @@
-"""
-이상한 문자 만들기 
-
-각 단어는 하나이상의 공백문자로 구분됨: 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자 
-
-"try hello world"	"TrY HeLlO WoRlD"
-"""
-
-
-s=input()
-
-word_list=list(s.split(' '))
-
-print(word_list)
-
-change_list=[]
-for word in word_list:
-  change=''
-  for idx in range(len(word)):
-    if idx%2==0:
-      change+=word[idx].upper()
-    else:
-      change+=word[idx].lower()
-  change_list.append(change)
-
-result=''
-
-for sub_word in change_list:
-  result+=sub_word
-  result+=' '
-result=result.rstrip()
-
-print(result)
+def solution(s):
+    word_list=list(s.split(' '))
+    change_list=[]
+    answer=''
+    for word in word_list:
+        for idx in range(len(word)):
+            if idx%2==0:
+                answer+=word[idx].upper()
+            else:
+                answer+=word[idx].lower()
+        answer+=' '
+    
+    answer =answer[:-1]
+    return answer
